655842f09cdd802e8ec217017e1e3d90f56f3e1c
youssef0999 <youssefahmed.ya@users.noreply.github.com>
2020-03-16 02:09:55 +0200

    -listview now toasts the weight selected and redirects back to the Rankings fragment

--- app/src/main/java/com/example/rnkery/WeightsFragment.kt
+++ app/src/main/java/com/example/rnkery/WeightsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.rnkery
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.CheckBox
 import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class WeightsFragment : Fragment() {
@@ -18,13 +19,26 @@ class WeightsFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_weight, null)
         var mweights = arrayOf("58- Kg", "68- Kg", "80- Kg", "87+ Kg")
         var fweights = arrayOf("49- Kg", "59- Kg", "67- Kg", "74+ Kg")
+        val checkbox = rootView.findViewById<CheckBox>(R.id.cb)
         val mylistview = rootView.findViewById(R.id.mylistview) as ListView
-        mylistview!!.adapter =
+        mylistview.adapter =
             ArrayAdapter<String>(activity!!, R.layout.weightslistlayout, R.id.tv, mweights)
 
-        rootView.setOnClickListener { view ->
-            Log.d("btnSetup", "Selected")
+        mylistview.setOnItemClickListener { adapterView,
+                                            view,
+                                            position,
+                                            l
+            ->
+            Toast.makeText(activity, mweights[position], Toast.LENGTH_SHORT).show()
+            val fragment = RankingsFragment()
+            val fragmentManager = activity!!.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+
         }
+
 
         return rootView
     }
